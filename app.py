17328f4171fb8c88065a66bc4321387b3905d666
Alexander Mamaev <alxmamaev@ya.ru>
2020-02-08 17:41:15 +0300

    Add debug logging

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def get_random_filename():
 
 @app.route("/process_photo", methods=['POST'])
 def process_photo():
+    print("starting processing")
     file = flask.request.files.get('photo', '')
     photo_content = file.read()
 
@@ -46,6 +47,7 @@ def process_photo():
             f.write(photo_content)
         response["filename"] = filename
 
+    print("done")
     return jsonify(response)
 
 
