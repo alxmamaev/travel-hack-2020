499c14115c4dbf2f2afe0ff9afd43aaf6ceade7a
Alexander Mamaev <alxmamaev@ya.ru>
2020-02-08 16:44:55 +0300

    Add images cache

--- app.py
+++ app.py
@@ -6,16 +6,24 @@ from google.cloud import vision
 from google.cloud.vision import types
 from flask import jsonify
 from flask_cors import CORS
+from random import choice
+from string import ascii_lowercase
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "./uploads"
 CORS(app)
 client = vision.ImageAnnotatorClient()
 
 
+def get_random_filename():
+    return "".join([choice(ascii_lowercase) for i in range(15)]) + ".jpg"
+
+
 @app.route("/process_photo", methods=['POST'])
 def process_photo():
-    photo_content = flask.request.files.get('photo', '').read()
+    file = flask.request.files.get('photo', '')
+    photo_content = file.read()
 
 
     photo = types.Image(content=photo_content)
@@ -23,7 +31,7 @@ def process_photo():
     labels_response = client.label_detection(image=photo)
 
 
-    response = {"landmarks": "", "is_selfie": False}
+    response = {"landmarks": "", "is_selfie": False, "filename":""}
 
 
     if landmark_response:
@@ -32,8 +40,18 @@ def process_photo():
         for label in labels_response.label_annotations:
             if label.description in ["Selfie", "Tourism"]:
                 response["is_selfie"] = True
-    
+
+        filename = get_random_filename()
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], filename), "wb") as f:
+            f.write(photo_content)
+        response["filename"] = filename
+
     return jsonify(response)
+
+
+@app.route('/img/<path:filename>') 
+def send_file(filename): 
+    return flask.send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == "__main__":
